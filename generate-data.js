665b39a38cc067a780b04f3f892d8c32624269ef
Gin <gin.milo.1901@gmail.com>
2021-04-02 05:56:57 +0700

    first commit

---
+++ generate-data.js
@@ -0,0 +1,80 @@
+const { random } = require('faker');
+const faker = require('faker');
+const fs = require('fs');
+// set locale to use Viet Nam
+
+faker.locale = 'vi';
+
+// Random data 
+// console.log(faker.commerce.department());
+// console.log(faker.commerce.productName());
+// console.log(faker.commerce.productDescription());
+
+// console.log(faker.image.imageUrl());
+// console.log(faker.name.findName());
+
+const randomCategoryList = (n) => {
+    if(n <= 0) return [];
+    const categoryList = []
+
+    // loop and push new data
+    Array.from(new Array(n)).forEach(() => {
+        const category = {
+            id: faker.random.uuid(),
+            name: faker.commerce.department(),
+            createAt: Date.now(),
+            createAt: Date.now(),
+        }; 
+
+        categoryList.push(category);
+    })
+    return categoryList;
+}
+
+const randomProductList = (categoryList, numberOfProducts) => {
+
+    if(numberOfProducts <= 0) return [];
+    const productList = [];
+
+    // random data
+    for (const category of categoryList){
+        Array.from(new Array(numberOfProducts)).forEach(() => {
+            const product = {
+                categoryId : category.id,
+                id: faker.random.uuid(),
+                name: faker.commerce.productName(),
+                color: faker.commerce.color(),
+                price: Number.parseFloat(faker.commerce.price()),
+                description: faker.commerce.productDescription(),
+                createAt: Date.now(),
+                updateAt: Date.now(),
+                thumbnailUrl: faker.image.imageUrl(400, 400),
+            };
+
+            productList.push(product);
+        })
+    }
+    return productList;
+}
+
+// IFFE
+(() => {
+
+    // random data
+    const categoryList = randomCategoryList(4);
+    const productList = randomProductList(categoryList, 5);
+
+    //prepare db object
+    const db = {
+        categories : categoryList,
+        products: productList,
+        profile: {
+            name: "Gin"
+        }
+    };
+
+    // write db object to db
+    fs.writeFile('db.json', JSON.stringify(db), () => {
+        console.log('Generate data successfully =))');
+    })
+})();
